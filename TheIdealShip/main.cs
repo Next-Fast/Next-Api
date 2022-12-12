0f51a2300e75201e9bff9902bd8388600659d728
MC_huier_lgsn <107238739+MC-AS-Huier@users.noreply.github.com>
2022-12-12 22:39:34 +0800

    1

--- TheIdealShip/main.cs
+++ TheIdealShip/main.cs
@@ -3,19 +3,22 @@ using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using Reactor;
-using Reactor.Utilities;
+using System;
+using Reactor.Networking.Attributes;
+using UnityEngine;
 
 namespace TheIdealShip;
 
-[BepInAutoPlugin(Id,ModName,ModVersion)]
+[BepInPlugin(Id, "The Ideal Ship", VersionString)]
 [BepInProcess("Among Us.exe")]
 [BepInDependency(ReactorPlugin.Id)]
+[ReactorModFlags(Reactor.Networking.ModFlags.RequireOnAllClients)]
 public partial class TheIdealShipPlugin : BasePlugin
 {
     public const string Id = "me.huier.TheIdealShip";
-    public const string ModName ="The Ideal Ship";
-    public const string ModVersion ="0.0.1";
-    public Harmony Harmony { get; } = new(Id);
+    public const string VersionString ="1.0.0";
+    public static Version Version = Version.Parse(VersionString);
+    public Harmony Harmony { get; } = new Harmony(Id);
 
     public ConfigEntry<string> ConfigName { get; private set; }
 
