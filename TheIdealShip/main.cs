fe41c63972806b4a45a1156a5e7e98bc62e6afec
MC_huier_lgsn <107238739+MC-AS-Huier@users.noreply.github.com>
2022-12-22 19:13:58 +0800

    1

--- TheIdealShip/main.cs
+++ TheIdealShip/main.cs
@@ -14,11 +14,17 @@ namespace TheIdealShip;
 //[ReactorModFlags(Reactor.Networking.ModFlags.RequireOnAllClients)]
 public partial class TheIdealShipPlugin : BasePlugin
 {
+    // Among Us游玩版本
     public const string AmongUsVersion = "12.8";
+    // 模组名称
     public const string ModName = "The Ideal Ship";
+    // 模组id
     public const string Id = "me.huier.TheIdealShip";
+    // 模组版本
     public const string VersionString = "1.0.0";
+    // 模组构建时间
     public const string BuildTime = "";
+    // 是否为开发版本
     public const bool IsDev = true;
     public static Version Version = Version.Parse(VersionString);
     public Harmony Harmony { get; } = new Harmony(Id);
