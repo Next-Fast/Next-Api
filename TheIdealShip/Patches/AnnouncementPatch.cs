1a65840c8e6a39b5cddcb7e3f0d38987cb2537bd
MC_huier_lgsn <107238739+MC-AS-Huier@users.noreply.github.com>
2023-03-02 12:17:14 +0800

    1

--- TheIdealShip/Patches/AnnouncementPatch.cs
+++ TheIdealShip/Patches/AnnouncementPatch.cs
@@ -1,22 +1,40 @@
-// using System;
-// using HarmonyLib;
-// using System.Net.Http;
-// using Newtonsoft.Json.Linq;
-// using TheIdealShip.Modules;
-//
-// namespace TheIdealShip.Patches
-// {
-//
-//     [HarmonyPatch(typeof(AnnouncementPopUp),nameof(AnnouncementPopUp.UpdateAnnounceText))]
-//     public static class AnnouncementPatch
-//     {
-//         public static string AMText = "我是一个大笨蛋";
-//         public static bool Prefix(AnnouncementPopUp __instance)
-//         {
-// //            if (!ModUpdater.HUpdate) return true;
-//             __instance.AnnounceTextMeshPro.text = AMText;
-//             return false;
-//         }
-//
-//     }
-// }
+/* using HarmonyLib;
+using TheIdealShip.Modules;
+using static TheIdealShip.Helpers;
+using AmongUs.Data;
+using Announcement = Assets.InnerNet.Announcement;
+using Il2CppSystem.Collections.Generic;
+
+namespace TheIdealShip.Patches
+{
+    [HarmonyPatch]
+    public class AnnouncementPatch
+    {
+        [HarmonyPatch(typeof(AnnouncementPopUp), nameof(AnnouncementPopUp.CreateAnnouncementList))]
+        public class AnnouncementCreateListPatch
+        {
+            private static List<Announcement> AnList = new List<Announcement>();
+            public static void Pfix()
+            {
+                var allan = DataManager.Player.Announcements.AllAnnouncements;
+/*                 if (!ModUpdater.HUpdate) return;
+                Announcement an = new Announcement();
+                an.Id = allan[0].Id + 1;
+                an.Language = allan[0].Language;
+                an.Number = 1;
+                an.Title = "TheIdealShip更新公告";
+                an.SubTitle = "测试而已";
+                an.ShortTitle = an.Title;
+                an.PinState = allan[0].PinState;
+                an.Text = "测试而已";
+                an.Date = "2023.3.1";
+                AnList.Add(allan[0]);
+                DataManager.Player.Announcements.AllAnnouncements.Clear();
+                foreach (var r in AnList)
+                {
+                    DataManager.Player.Announcements.AllAnnouncements.Add(r);
+                }
+            }
+        }
+    }
+} */
