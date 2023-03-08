bc05a146b2d1ce51dab5a8346551c89ef4563820
MC_huier_lgsn <107238739+MC-AS-Huier@users.noreply.github.com>
2023-03-09 06:24:26 +0800

    RPC更新

--- TheIdealShip/RPC/RPCHelpers.cs
+++ TheIdealShip/RPC/RPCHelpers.cs
@@ -1,3 +1,4 @@
+using System;
 using Hazel;
 using TheIdealShip.Utilities;
 
@@ -34,78 +35,183 @@ public static class RPCHelpers
 
         if (strings != null) { foreach (var str in strings) { rpcStart.Write(str); } }
 
+//        SendValueLength(bytes.Length, ints.Length, bools.Length, floats.Length, strings.Length);
+
         AmongUsClient.Instance.FinishRpcImmediately(rpcStart);
     }
 
-    public static void ReadRPCValue()
+    public static void ReadValue(MessageReader reader)
     {
+        for (var b = 0; b < ReadRPCValue.byteL; b++)
+        {
+            byte ByteValue = reader.ReadByte();
+            ReadRPCValue.bytes[b] = ByteValue;
+        }
 
+        for (var i = 0; i < ReadRPCValue.intL; i++)
+        {
+            int IntValue = reader.ReadInt32();
+            ReadRPCValue.ints[i] = IntValue;
+        }
+
+        for (var bo = 0; bo < ReadRPCValue.boolL; bo++)
+        {
+            bool BoolValue = reader.ReadBoolean();
+            ReadRPCValue.bools[bo] = BoolValue;
+        }
+
+        for (var f = 0; f < ReadRPCValue.floatL; f++)
+        {
+            float FloatValue = reader.ReadSingle();
+            ReadRPCValue.floats[f] = FloatValue;
+        }
+
+        for (var s = 0; s < ReadRPCValue.stringL; s++)
+        {
+            string StringValue = reader.ReadString();
+            ReadRPCValue.strings[s] = StringValue;
+        }
     }
 
-    // public static void StartRPC(byte rpc, MessageReader reader = null)
-    // {
-    //     switch (rpc)
-    //     {
-    //         case (byte)CustomRPC.ResetVariables:
-    //             RPCProcedure.ResetVariables();
-    //             break;
-    //
-    //         case (byte)CustomRPC.WorkaroundSetRoles:
-    //             RPCProcedure.WorkaroundSetRoles(reader.ReadByte(), reader);
-    //             break;
-    //
-    //         case (byte)CustomRPC.SetRole:
-    //             byte roleId = reader.ReadByte();
-    //             byte playerId = reader.ReadByte();
-    //             RPCProcedure.setRole(roleId, playerId);
-    //             break;
-    //
-    //         case (byte)CustomRPC.SetModifier:
-    //             byte modifierId = reader.ReadByte();
-    //             byte pId = reader.ReadByte();
-    //             byte flag = reader.ReadByte();
-    //             RPCProcedure.setModifier(modifierId, pId, flag);
-    //             break;
-    //
-    //         case (byte)CustomRPC.setDead:
-    //             byte id = reader.ReadByte();
-    //             bool isDead = reader.ReadBoolean();
-    //             RPCProcedure.setDead(id, isDead);
-    //             break;
-    //
-    //         case (byte)CustomRPC.SheriffKill:
-    //             byte targetId = reader.ReadByte();
-    //             RPCProcedure.SheriffKill(targetId);
-    //             break;
-    //         
-    //         case (byte)CustomRPC.RestoreRole :
-    //             byte RestoreRoleId = reader.ReadByte();
-    //             RPCProcedure.RestoreRole(RestoreRoleId);
-    //             break;
-    //         
-    //         case (byte)CustomRPC.RestorePlayerLook :
-    //             RPCProcedure.RestorePlayerLook();
-    //             break;
-    //         
-    //         case (byte)CustomRPC.ChangeRole :
-    //             byte ChangeRolePlayerId = reader.ReadByte();
-    //             byte ChangeRoleTargetRoleId = reader.ReadByte();
-    //             RPCProcedure.ChangeRole(ChangeRolePlayerId, ChangeRoleTargetRoleId);
-    //             break;
-    //         
-    //         case (byte)CustomRPC.Camouflager :
-    //             RPCProcedure.Camouflager();
-    //             break;
-    //         
-    //         case (byte)CustomRPC.Illusory :
-    //             RPCProcedure.Illusory();
-    //             break;
-    //         
-    //         case (byte)CustomRPC.customrpc:
-    //             reader.Recycle();
-    //             break;
-    //     }
-    // }
+    public static void ReadValueLength(MessageReader reader)
+    {
+        ReadRPCValue.ClearAll();
+        for (int length = reader.ReadInt32(); length == 0; length--)
+        {
+            byte type = reader.ReadByte();
+            switch (type)
+            {
+                case (byte)ReadType.Byte:
+                    ReadRPCValue.byteL = reader.ReadInt32();
+                    break;
+
+                case (byte)ReadType.Int:
+                    ReadRPCValue.intL = reader.ReadInt32();
+                    break;
+
+                case (byte)ReadType.Bool:
+                    ReadRPCValue.boolL = reader.ReadInt32();
+                    break;
+
+                case (byte)ReadType.Float:
+                    ReadRPCValue.floatL = reader.ReadInt32();
+                    break;
+
+                case (byte)ReadType.String:
+                    ReadRPCValue.stringL = reader.ReadInt32();
+                    break;
+            }
+        }
+        ReadValue(reader);
+    }
+
+    public static void SendValueLength(int byteL = 0, int intL = 0, int boolL = 0, int FloatL = 0, int StringL = 0)
+    {
+        MessageWriter rpcStart = AmongUsClient.Instance.StartRpcImmediately(CachedPlayer.LocalPlayer.PlayerControl.NetId, (byte)CustomRPC.RpcValueHandshake, SendOption.Reliable);
+        int Length = 0;
+        if (byteL != 0) Length++;
+        if (intL != 0) Length++;
+        if (boolL != 0) Length++;
+        if (FloatL != 0) Length++;
+        if (StringL != 0) Length++;
+        rpcStart.Write(Length);
+
+        if (byteL != 0)
+        {
+            rpcStart.Write((byte)ReadType.Byte);
+            rpcStart.Write(byteL);
+        }
+
+        if (intL != 0)
+        {
+            rpcStart.Write((byte)ReadType.Int);
+            rpcStart.Write(intL);
+        }
+
+        if (boolL != 0)
+        {
+            rpcStart.Write((byte)ReadType.Bool);
+            rpcStart.Write(boolL);
+        }
+
+        if (FloatL != 0)
+        {
+            rpcStart.Write((byte)ReadType.Float);
+            rpcStart.Write(FloatL);
+        }
+
+        if (StringL != 0)
+        {
+            rpcStart.Write((byte)ReadType.String);
+            rpcStart.Write(StringL);
+        }
+        AmongUsClient.Instance.FinishRpcImmediately(rpcStart);
+    }
+
+    public static void StartRPC(byte rpc, MessageReader reader = null, byte[] bytes = null, string[] strings = null, bool[] bools = null )
+    {
+        switch (rpc)
+        {
+            case (byte)CustomRPC.ResetVariables:
+                RPCProcedure.ResetVariables();
+                break;
+
+            case (byte)CustomRPC.WorkaroundSetRoles:
+                RPCProcedure.WorkaroundSetRoles(reader.ReadByte(), reader);
+                break;
+
+            case (byte)CustomRPC.SetRole:
+                RPCProcedure.setRole(reader.ReadByte(), reader.ReadByte());
+                break;
+
+            case (byte)CustomRPC.SetModifier:
+                RPCProcedure.setModifier(reader.ReadByte(), reader.ReadByte(), reader.ReadByte());
+                break;
+
+            case (byte)CustomRPC.setDead:
+                RPCProcedure.setDead(reader.ReadByte(), reader.ReadBoolean());
+                break;
+
+            case (byte)CustomRPC.SheriffKill:
+                RPCProcedure.SheriffKill(reader.ReadByte());
+                break;
+
+            case (byte)CustomRPC.RestoreRole :
+                byte RestoreRoleId = reader.ReadByte();
+                RPCProcedure.RestoreRole(RestoreRoleId);
+                break;
+
+            case (byte)CustomRPC.RestorePlayerLook :
+                RPCProcedure.RestorePlayerLook();
+                break;
+
+            case (byte)CustomRPC.HistorySynchronization:
+                RPCProcedure.HistorySynchronization(reader.ReadByte(), reader.ReadInt32(), reader.ReadByte(), reader.ReadByte());
+                break;
+
+            case (byte)CustomRPC.ChangeRole :
+                RPCProcedure.ChangeRole(reader.ReadByte(), reader.ReadByte());
+                break;
+
+            case (byte)CustomRPC.Camouflager :
+                RPCProcedure.Camouflager();
+                break;
+
+            case (byte)CustomRPC.Illusory :
+                RPCProcedure.Illusory();
+                break;
+
+            case (byte)CustomRPC.SchrodingerSCatTeamChange:
+                RPCProcedure.SchrodingerSCatTeamChange(reader.ReadByte());
+                break;
+
+            case (byte)CustomRPC.LoverSendChat:
+                PlayerControl sendChatPlayer = Helpers.GetPlayerForId(reader.ReadByte());
+                string ChatText = reader.ReadString();
+                RPCProcedure.LoverSendChat(sendChatPlayer, ChatText);
+                break;
+        }
+    }
 }
 public static class ReadRPCValue
 {
@@ -114,4 +220,30 @@ public static class ReadRPCValue
     public static bool[] bools;
     public static float[] floats;
     public static string[] strings;
+    public static int byteL;
+    public static int intL;
+    public static int boolL;
+    public static int floatL;
+    public static int stringL;
+    public static void ClearAll()
+    {
+        bytes = null;
+        ints = null;
+        bools = null;
+        floats = null;
+        strings = null;
+        byteL = 0;
+        intL = 0;
+        boolL = 0;
+        floatL = 0;
+        stringL = 0;
+    }
+}
+public enum ReadType
+{
+    Byte,
+    Int,
+    Bool,
+    Float,
+    String
 }
