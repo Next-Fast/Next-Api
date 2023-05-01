cf667227bf542e65a05d2e79b04f78e874f43e72
MC_huier_lgsn <107238739+MC-AS-Huier@users.noreply.github.com>
2023-05-01 19:32:34 +0800

    小写一些

--- TheIdealShip/Roles/RoleBase.cs
+++ TheIdealShip/Roles/RoleBase.cs
@@ -1,12 +1,20 @@
+global using RoleBase = TheIdealShip.Roles.RoleBase;
+
+using System.Collections.Generic;
 namespace TheIdealShip.Roles;
 
 public class RoleBase
 {
+    public static List<RoleBase> RoleBaseS = new List<RoleBase>();
+
     public string RoleName { get; }
     public RoleId roleid { get; }
     public bool CanKill { get; }
     public bool CanVent { get; }
     public bool HasTask { get; }
+
+    public List<CustomButton> Buttons = new List<CustomButton>();
+    public List<CustomOption> Options = new List<CustomOption>();
 
     public RoleBase(string role, RoleId roleId)
     {
@@ -15,5 +23,20 @@ public class RoleBase
         CanKill = false;
         CanVent = false;
         HasTask = true;
+
+        Buttons = null;
+        Options = null;
+
+        RoleBaseS.Add(this);
+    }
+}
+
+public static class RoleBaseVoid
+{
+
+    public static RoleBase GetRoleBase(this RoleId id)
+    {
+        RoleBase roleBase = RoleBase.RoleBaseS.Find(x => x.roleid == id);
+        return roleBase;
     }
 }
