f3fd48e220ec223bf0e33474f9c716e1730261a0
MC_huier_lgsn <107238739+MC-AS-Huier@users.noreply.github.com>
2023-06-22 04:44:39 +0800

    GameObjectUtils

--- TheIdealShip/Languages/LanguageCSV.cs
+++ TheIdealShip/Languages/LanguageCSV.cs
@@ -101,7 +101,7 @@ namespace TheIdealShip.Languages
             {
                 res = $"*{str}";
             }
-            
+
             return res;
         }
     }
---
+++ TheIdealShip/Utils/GameObjectUtils.cs
@@ -0,0 +1,31 @@
+
+using HarmonyLib;
+using UnityEngine;
+
+namespace TheIdealShip;
+
+public static class GameObjectUtils
+{
+    // form TOHE
+    /// <summary>
+    /// 删除翻译文本（<see cref="TextTranslatorTMP"/>）组件
+    /// </summary>
+    public static void DestroyTranslator(this GameObject obj)
+    {
+        TextTranslatorTMP[] translatorTMPs = obj.GetComponentsInChildren<TextTranslatorTMP>(true);
+        if (translatorTMPs != null) translatorTMPs.Do(n => GameObject.Destroy(n));
+    }
+    /// <summary>
+    /// 删除翻译文本（<see cref="TextTranslatorTMP"/>）组件
+    /// </summary>
+    public static void DestroyTranslator(this MonoBehaviour obj) => obj.gameObject.DestroyTranslator();
+
+    /// <summary>
+    /// 删除纠正位置（<see cref="AspectPosition"/>）组件
+    /// </summary>
+    public static void DestroyAspectPosition(this GameObject gameObject)
+    {
+        AspectPosition[] aspectPositions = gameObject.GetComponentsInChildren<AspectPosition>(true);
+        if (aspectPositions != null) aspectPositions.Do(n => GameObject.Destroy(n));
+    }
+}
